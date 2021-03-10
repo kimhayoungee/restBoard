19bc22b20681fc5aa786be78630a5cd4e4e73c94
ha0ppy <kimhayoungee@gmail.com>
2021-03-11 01:09:37 +0900

    ReplyMapperTest 확인

---
+++ board/src/main/java/com/my/mapper/ReplyMapper.java
@@ -0,0 +1,10 @@
+package com.my.mapper;
+
+import com.my.domain.ReplyVO;
+
+public interface ReplyMapper {
+
+	public int insert(ReplyVO rvo);
+	
+	
+} //end of ReplyMapper
---
+++ board/src/test/java/com/my/mapper/ReplyMapperTest.java
@@ -0,0 +1,25 @@
+package com.my.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class ReplyMapperTest {
+
+	@Autowired
+	private ReplyMapper rm;
+	
+	@Test
+	public void testMapper() {
+		log.info(rm);
+	}
+	
+	
+}
