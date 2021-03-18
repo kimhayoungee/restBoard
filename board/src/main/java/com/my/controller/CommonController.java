82bdfa8d88a38735f4838484199d37f362f8b89d
ha0ppy <kimhayoungee@gmail.com>
2021-03-19 01:08:51 +0900

    로그인,아웃 확인

---
+++ board/src/main/java/com/my/controller/CommonController.java
@@ -0,0 +1,21 @@
+package com.my.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class CommonController {
+
+	@GetMapping("/accessError")
+	public void accessDenied(Authentication auth, Model model) {
+		log.info("access Denied: " + auth);
+		
+		model.addAttribute("msg", "Access Denied");
+	}
+	
+} //end of CommonController
